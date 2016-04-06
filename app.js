255705034900a82249113839fa361a95a51c4f0c
Kate Eldridge <kathryneeldridge@gmail.com>
2016-04-05 23:24:15 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+//defined app
+var app = angular.module('flapperNews', []);
+
+//defined main ctrl
+app.controller('MainCtrl', [
+//defined scope
+	'$scope',
+	function($scope){
+	//defined test
+		$scope.test ='hello world';
+		$scope.posts = [
+			{title:'post 1', upvotes:5},
+			{title:'post 2', upvotes:3},
+			{title:'post 3', upvotes:3},
+			{title:'post 4', upvotes:6},
+			{title:'post 5', upvotes:4}
+		];
+		$scope.addPost = function(){
+			if(!$scope.title || $scope.title === ' ') {return; }
+			$scope.posts.push({
+			title: $scope.title, 
+			link:$scope.link,
+			upvotes:0
+		});
+			$scope.title = '';
+			$scope.link = '';
+		};
+		$scope.incrementUpvotes = function(post){
+			post.upvotes += 1;
+		};
+	}]);

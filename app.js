8c437f03e3498944188f84e263bb70ad39ef5488
Kate Eldridge <kathryneeldridge@gmail.com>
2016-04-20 18:12:13 -0400

    created new postCtrl in app.js

--- app.js
+++ app.js
@@ -38,16 +38,23 @@ app.controller('MainCtrl', [
 		$scope.posts = posts.posts;
 		$scope.addPost = function(){
 			if(!$scope.title || $scope.title === ' ') {return; }
-			$scope.posts.push(
-				{
-					title: $scope.title, 
-					link:$scope.link,
-					upvotes:0
-				});
+			$scope.posts.push({
+				title: $scope.title, 
+				link:$scope.link,
+				upvotes:0,
+				comments:[]
+			});
 			$scope.title = '';
 			$scope.link = '';
 		};
 		$scope.incrementUpvotes = function(post){
 			post.upvotes += 1;
 		};
+	}]);
+
+//define posts ctrl
+app.controller('PostsCtrl',[
+	'$scope', '$stateParams', 'posts', 
+	function($scope, $stateParams, posts){
+
 	}]);
